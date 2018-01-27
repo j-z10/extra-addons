ab8a1e3770d56a901882ea8cb5539c01e1d81aab
ZhangJie <zhangj_10ujs@foxmail.com>
2018-01-27 14:11:10 +0800

    Basic workflow backend support

---
+++ workflow/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'workflow',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'description': """
+Trans workflow on odoo-10 to odoo-11.0
+""",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/workflow_view.xml',
+    ],
+    'installable': True,
+}
---
+++ workflow/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import workflow
+from . import workflow_mixin
