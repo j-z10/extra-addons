5e8474153e6a5876bc85ba64b459d64607b52c8e
ZhangJie <zhangj_10ujs@outlook.com>
2018-02-03 15:47:10 +0800

    A 添加模块，可以移除登陆界面的odoo链接

---
+++ remove_login_odoo_link/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "remove_login_odoo_link",
+
+    'summary': """移除登陆界面的Powered by Odoo的链接""",
+
+    'description': """移除登陆界面的Powered by Odoo的链接""",
+
+    'author': "ZhangJie",
+
+    'category': 'Tools',
+    'version': '0.1',
+
+    'depends': ['web'],
+
+    'data': [
+        'templates.xml',
+    ],
+}
