30fd8bbc3b0f261d12118fef5ebe78689689a669
ZhangJie <zhangj_10ujs@foxmail.com>
2018-06-30 15:34:11 +0800

    两种下载方式

---
+++ client_download/Readme.md
@@ -0,0 +1,6 @@
+html table data to csv or xls file
+----
+
+- 1. in any action context add `{'client_download_type': 'csv'}` or `{'client_download_type': 'xls'}`
+- 2. go to the action's list view, there will be a download button after create
+- 3. click it, then the current page of data will be download
--- client_download/static/src/list_controller.js
+++ client_download/static/src/list_controller.js
@@ -5,14 +5,17 @@ var ListController = require('web.ListController');
 var field_utils = require('web.field_utils');
 
 ListController.include({
+    init: function(parent, model, renderer, params){
+        this._super.apply(this, arguments);
+        this.client_download_type = params.initialState.context.client_download_type;
+    },
     renderButtons: function ($node) {
         this._super.apply(this, arguments);
         if (!this.noLeaf && this.hasButtons) {
             this.$buttons.on('click', '.o_list_button_download', this._onButtonDownload.bind(this));
         }
     },
-    _onButtonDownload: function() {
-        console.log('self is', this);
+    _getPageData: function(){
         var self = this;
         var headers = this.renderer.columns.map(function(v){return v.attrs.name});
         var rows = this.renderer.state.data.map(function(record){
@@ -26,10 +29,16 @@ ListController.include({
             });
         });
         var data = [];
-        data.push(headers);
+        data.push(headers.map(function(n){return self.renderer.state.fields[n].string}));
         data.push.apply(data, rows);
-        this.exportToCsv(this.modelName + '.csv', data);
-
+        return data;
+    },
+    _onButtonDownload: function() {
+        if (this.client_download_type === 'csv') {
+            return this.exportToCsv(this.modelName + '.csv', this._getPageData());
+        } else {
+            return this.exportToExcel(this.modelName + '.xls');
+        }
     },
     exportToCsv: function (filename, rows) {
         var processRow = function (row) {
@@ -55,7 +64,39 @@ ListController.include({
             csvFile += processRow(rows[i]);
         }
 
-        var blob = new Blob([csvFile], {type: 'text/csv;charset=utf-8;'});
+        return this.exportDownload(filename, csvFile, {type: 'text/csv;charset=utf-8;'});
+    },
+    exportToExcel: function (filename) {
+        var tab_text = "";
+        var tab = this.$el.find('table')[0];
+        if (!tab) {
+            return;
+        }
+        if (tab.tHead) {
+            tab_text += '<tr bgcolor=\'#87AFC6\'>' + tab.rows[0].innerHTML+"</tr>";
+        }
+        for(var j=1; j < tab.rows.length ; j++) {
+            tab_text += '<tr>' + tab.rows[j].innerHTML+"</tr>";
+        }
+        tab_text= tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+        tab_text= tab_text.replace(/<img[^>]*>/gi,""); // remove if u want images in your table
+        tab_text= tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+        var
+            template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table border="2px">{table}</table></body></html>',
+            format = function (s, c) {
+                return s.replace(/{(\w+)}/g, function (m, p) {
+                    return c[p];
+                })
+            };
+        var ctx = {
+            worksheet: "Sheet 1" || 'Worksheet',
+            table: tab_text
+        };
+        return this.exportDownload(filename, format(template, ctx), {type: 'application/vnd.ms-excel'});
+    },
+    exportDownload: function(filename, data, option){
+        var blob = new Blob([data], option);
         if (navigator.msSaveBlob) { // IE 10+
             navigator.msSaveBlob(blob, filename);
         } else {
